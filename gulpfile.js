3572b7e4cf60a73a57e56100e98ef9c2ca350d0d
evgenskaspl <evgenskaspl@gmail.com>
2023-12-14 07:53:54 +0100

    Add header, menu, banner and about us section

--- gulpfile.js
+++ gulpfile.js
@@ -19,18 +19,15 @@ const changed = require('gulp-changed');
 const browsersync = require('browser-sync').create();
 const template = require('gulp-template');
 
-console.log('test', process.env.NODE_ENV)
-const isDev = process.env.NODE_ENV === 'development'
-
 // gulp config
 const { htmlName, paths, port } = {
     port: 3000,
     htmlName: 'index.html',
     paths: {
-        baseDir: './',
-        htmlTemplate: './index_template.html',
-        css: isDev ? 'dist/css/main.min.css' : 'css/main.min.css',
-        js: isDev ? 'dist/js/bundle.min.js' : 'js/bundle.min.js'
+        baseDir: './dist',
+        htmlTemplate: './src/index_template.html',
+        css: 'css/main.min.css',
+        js: 'js/bundle.min.js'
     }
 }
 
@@ -41,26 +38,16 @@ function clear() {
         .pipe(clean());
 }
 
-
-
-function devHtml() {
-    return src(paths.htmlTemplate)
-        .pipe(template({ cssPath: paths.css, jsPath: paths.js }))
-        .pipe(rename(htmlName))
-        .pipe(dest(paths.baseDir))
-        .pipe(browsersync.stream());
-};
-
-
 function html() {
     return src(paths.htmlTemplate)
         .pipe(template({ cssPath: paths.css, jsPath: paths.js }))
         .pipe(rename(htmlName))
         .pipe(dest('dist'))
+        .pipe(browsersync.stream());
 };
 
 function css() {
-    const source = './src/scss/*.scss';
+    const source = 'src/scss/*.scss';
 
     return src(source)
         .pipe(sass())
@@ -71,13 +58,13 @@ function css() {
         .pipe(rename({
             extname: '.min.css'
         }))
-        .pipe(cssnano())
+        // .pipe(cssnano())
         .pipe(dest('./dist/css/'))
         .pipe(browsersync.stream());
 }
 
 function js() {
-    const source = './src/js/*.js';
+    const source = 'src/js/*.js';
 
     return src(source)
         .pipe(changed(source))
@@ -91,16 +78,28 @@ function js() {
 }
 
 function img() {
-    return src('./src/img/*')
+    return src('src/img/*')
         .pipe(imagemin())
         .pipe(dest('./dist/img'));
 }
 
+function copyFonts() {
+    return src('src/fonts/*')
+        .pipe(imagemin())
+        .pipe(dest('dist/fonts'));
+}
+
+function copyFavicon() {
+    return src('src/favicon.ico')
+        .pipe(imagemin())
+        .pipe(dest('dist'));
+}
+
 function watchFiles() {
-    watch('./src/scss/*', css);
-    watch('./src/js/*', js);
-    watch('./src/img/*', img);
-    watch(paths.htmlTemplate, devHtml);
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/*', js);
+    watch('src/img/*', img);
+    watch(paths.htmlTemplate, html);
 }
 
 function browserSync() {
@@ -114,5 +113,7 @@ function browserSync() {
 
 // Tasks to define the execution of the functions simultaneously or in series
 
-exports.watch = parallel(devHtml, watchFiles, browserSync);
-exports.default = series(clear, parallel(html, js, css, img));
+const tools = [html, copyFonts, copyFavicon, js, css, img]
+
+exports.watch = parallel(...tools, watchFiles, browserSync);
+exports.default = series(clear, parallel(...tools));
