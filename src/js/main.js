5924e26c1ba7622abd8b1c521d3604c952818cba
evgenskaspl <evgenskaspl@gmail.com>
2024-01-06 17:48:50 +0100

    Uptimize images and small layout changes

--- src/js/main.js
+++ src/js/main.js
@@ -7,25 +7,12 @@ Fancybox.bind("[data-fancybox]", {
 
 window.addEventListener('load', function () {
     const images = document.querySelectorAll('.lazy-image')
-    console.log('images', images)
     for (let image of images) {
-        if (image.dataset.src) {
-            image.src = image.dataset.src
+        if (image.src) {
             image.style.display = "block";
-            console.log(image.parentNode.parentNode)
-            image.parentNode.parentNode.style.filter = 'none'
         }
+        image.addEventListener('load', function () {
+            image.parentNode.parentNode.style.filter = 'none'
+        })
     }
 })
-
-// Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "evgenskaspl@gmail.com",
-//     Password: "!qQ5063743",
-//     To: 'evgenskas@gmail.com',
-//     From: "test1234@gmail.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body"
-// }).then(
-//     message => console.log(message)
-// ).catch(e => console.log('e', e));
